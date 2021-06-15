9a84b3bd32075aefe06958f69da96a5bce5b9d65
stephanvdplas <47625764+stephanvdplas@users.noreply.github.com>
2021-06-15 15:34:08 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,36 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 5) {
+        for (let index = 0; index < 10; index++) {
+            basic.pause(500)
+            radio.sendNumber(randint(0, 4))
+        }
+    } else if (receivedNumber == 10) {
+        unlock = 15
+    } else if (receivedNumber == 15) {
+        music.playTone(262, music.beat(BeatFraction.Eighth))
+        music.rest(music.beat(BeatFraction.Eighth))
+        music.playTone(262, music.beat(BeatFraction.Eighth))
+        music.rest(music.beat(BeatFraction.Eighth))
+        music.playTone(262, music.beat(BeatFraction.Eighth))
+        music.rest(music.beat(BeatFraction.Eighth))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.playTone(523, music.beat(BeatFraction.Half))
+        music.rest(music.beat(BeatFraction.Sixteenth))
+        music.playTone(262, music.beat(BeatFraction.Eighth))
+        music.rest(music.beat(BeatFraction.Eighth))
+        music.playTone(262, music.beat(BeatFraction.Eighth))
+        music.rest(music.beat(BeatFraction.Eighth))
+        music.playTone(262, music.beat(BeatFraction.Eighth))
+        music.rest(music.beat(BeatFraction.Eighth))
+    } else {
+        basic.showNumber(receivedNumber)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(5)
+})
+let unlock = 0
+radio.setGroup(1)
